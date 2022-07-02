0d769f030dd34e027bc5d8b777d3b188ecc1d0d1
ruxandra <pepicoal@yahoo.com>
2022-07-02 18:08:04 +0300

    very weird unworking js

--- script.js
+++ script.js
@@ -1,28 +1,13 @@
 const addBook = document.getElementById("addBook");
 const submit = document.getElementById("submit");
-const form = document.getElementById("form");
 
-let toggleAddBook = 0;
+let removeRows = [];
 
-function checkToggleBook(toggle) {
-    if(toggleAddBook % 2 == 1) {
-        addBook.innerHTML = "-";
-    }
-    else {
-        addBook.innerHTML = "+";
-    }
-}
 
-addBook.addEventListener("click", () => {
-    form.classList.toggle("showForm");
-    toggleAddBook++;
-    checkToggleBook(toggleAddBook);
-})
 
 /////////
 /////////
 
-let tbody = document.getElementById("tbody");
 let title = document.getElementById("title");
 let author = document.getElementById("author");
 let pages = document.getElementById("pages");
@@ -32,11 +17,24 @@ let myLibrary = [];
 
 function Book(title, author, pages, status) {
     this.title = title;
-    
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
 }
 
+
+
+function removeRow(element) {
+    element.title = "";
+    element.author = "";
+    element.pages  = "";
+    element.status = "";
+}
+
+let bookAuthor, bookTitle, bookPages, bookStatus;
+
 function getBookData() {
-    let bookAuthor, bookTitle, bookPages, bookStatus;
+    
     if(title.value === "") return;
     bookTitle = title.value;
     if(author.value != "") {
@@ -58,7 +56,6 @@ function getBookData() {
     } else if(status.value == "finished") {
         bookStatus = "finished";
     }
-    console.log(bookAuthor, bookPages, bookStatus, bookTitle)
 }
 
 function resetForm() {
@@ -68,8 +65,116 @@ function resetForm() {
     status.value = "not-started";
 }
 
+let tbody = document.getElementById("tbody");
+const form = document.getElementById("form");
+
+let i = 0;
+let newTrashcan;
+function newRow() {
+    let row = document.createElement("tr");
+
+    row.id = `${i}`;
+
+    let newTitle = document.createElement("td");
+    row.appendChild(newTitle);
+    newTitle.innerText = myBook.title;
+    let newAuthor = document.createElement("td");
+    row.appendChild(newAuthor);
+    newAuthor.innerText = myBook.author;
+    let newPages = document.createElement("td");
+    row.appendChild(newPages);
+    newPages.innerText = myBook.pages;
+    let newStatus = document.createElement("td");
+    row.appendChild(newStatus);
+    newStatus.innerText = myBook.status;
+
+   // newTrashcan = document.createElement("td");
+   // row.appendChild(newTrashcan);
+   // newTrashcan.innerHTML = '<button class="remove"><img src="./img/trash.png"></button>';
+   //newTrashcan.innerText = "Remove!";
+  // newTrashcan.classList.add("remove")
+    tbody.appendChild(row);
+    //removeRows = document.querySelectorAll(".remove");
+    i++;
+}
+
+let myBook = "";
+
+//////EVENT LISTENERS
+
+removeRows.forEach(element => {
+    element.addEventListener("click", () => {
+        console.log("da")
+    })
+    
+});
+
 submit.addEventListener("click", (e) => {
     e.preventDefault();
     getBookData();
-    if(title.value != "") resetForm();
+    if(title.value != "") {
+        resetForm();
+        myBook = new Book(bookTitle, bookAuthor, bookPages, bookStatus);
+
+        myLibrary.push(myBook);
+        newRow();
+        //removeRows = document.querySelectorAll(".remove");
+
+        myBook = "";
+    }
+    
 })
+
+let toggleAddBook = 0;
+function checkToggleBook(toggle) {
+    if(toggleAddBook % 2 == 1) {
+        addBook.innerHTML = "-";
+    }
+    else {
+        addBook.innerHTML = "+";
+    }
+}
+addBook.addEventListener("click", () => {
+    form.classList.toggle("showForm");
+    toggleAddBook++;
+    checkToggleBook(toggleAddBook);
+})
+
+
+//////////////////////////////////////////////
+////TESTS ///
+////////////////// 
+/*
+const aRow = document.getElementById("0") ;
+
+
+const allCells = document.getElementsByTagName("td");
+Array.from(allCells).forEach(element => {
+    element.addEventListener("click", () => {
+        console.log("XXXXx")
+    })
+})
+*/
+
+/////////////////////////////////////////////////////////////////////
+////////////////////////NEW//////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+
+
+
+//////////////////
+
+
+///UI
+
+
+///LOCALSTORE
+
+
+///DISPAY
+
+
+///ADD BOOK
+
+
+///REMOVE BOOK
